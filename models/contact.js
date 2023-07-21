a9eb5ee378b6fdb8414e7d4f0ac8c7b490348ce3
Kateryna Verveda <katushka211@gmail.com>
2023-07-21 17:06:08 +0200

    update schema

--- models/contact.js
+++ models/contact.js
@@ -2,14 +2,11 @@ const { Schema, model } = require("mongoose");
 const { handleMongooseError } = require("../helpers");
 const Joi = require("joi");
 
-// const phoneRegexp = /^\(\d{3}\) \d{3}-\d{4}$/;
-
 const contactSchema = new Schema(
   {
     name: { type: String, required: [true, "Set name for contact"] },
     email: { type: String },
     phone: { type: String },
-    // phone: { type: String, match: phoneRegexp },
     favorite: { type: Boolean, default: false },
     owner: {
       type: Schema.Types.ObjectId,
@@ -26,7 +23,6 @@ const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
-  // phone: Joi.string().pattern(phoneRegexp).required(),
   favorite: Joi.boolean(),
 });
 
